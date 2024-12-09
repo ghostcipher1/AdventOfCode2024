41325c1edf2b214ecf236477c2b00a88dd8822ac
Trent Adams <adamstechrepository@gmail.com>
2024-12-08 20:19:25 -0500

    Added Day7 Challenge Part 1

---
+++ Day7.fs
@@ -0,0 +1,40 @@
+﻿module Day7
+open System
+
+
+let evaluateExpression (numbers: int64 list) (operators: char list) : int64 =
+    List.fold2 (fun acc num op -> 
+        match op with
+        | '+' -> acc + num
+        | '*' -> acc * num
+        | _ -> acc) (List.head numbers) (List.tail numbers) operators
+
+let generateOperatorCombinations length =
+    let rec combinations n acc =
+        if n = 0 then acc
+        else combinations (n - 1) ([for op in ['+'; '*'] do for comb in acc do yield op :: comb])
+    combinations length [[]]
+
+let findCalibrationTotal (lines: string list) : int64 =
+    lines
+    |> List.fold (fun totalCalibrationResult line ->
+        let parts = line.Split(':')
+        let target = Int64.Parse(parts.[0].Trim())
+        let numbers = parts.[1].Trim().Split(' ') |> Array.map Int64.Parse |> Array.toList
+
+        let operatorCombinations = generateOperatorCombinations (numbers.Length - 1)
+
+        let valid = operatorCombinations |> List.exists (fun operators -> evaluateExpression numbers operators = target)
+
+        if valid then totalCalibrationResult + target
+        else 
+            printfn "No valid operator combinations for target %d with numbers %A" target numbers
+            totalCalibrationResult
+    ) 0
+
+
+// Calculate the total calibration result
+let solve input = (input |> findCalibrationTotal, "")
+
+//let result = (findCalibrationTotal |> data)
+//printfn "Total Calibration Result: %d" input
---
+++ Utils.fs
@@ -0,0 +1,32 @@
+﻿module AOC2024.Utils
+open System
+open System.IO
+open System.Text
+
+// Function to read lines of text out of a file as a sequence
+let ReadInput fname = File.ReadLines fname
+
+// Function to extract a list of integers integers separated by non-digit 
+// characters from a string and turn them into a list.  
+let parseDigits line =
+    let buffer = StringBuilder()
+    let result = ResizeArray<int>()
+    let flushBuffer () =
+        if buffer.Length > 0 then
+            result.Add(int (buffer.ToString()))
+            buffer.Clear() |> ignore
+    line |> Seq.iter (fun c ->
+        if Char.IsDigit c then 
+            buffer.Append(c) |> ignore
+        else 
+            flushBuffer())
+    flushBuffer()
+    result |> Seq.toList
+
+
+// $ead the entire input into memory to form the grid as a two
+// dimensional list of chars
+let getGrid (input:seq<string>) = 
+    input 
+    |> Seq.toList 
+    |> List.map (fun s -> s.ToCharArray() |> Array.toList) 
